63795b2ceb309ffdf447992f1147a786a9845713
sk5596-collab <sk5596@srmist.edu.in>
2026-02-16 22:11:30 +0530

    oops banner in star

---
+++ OOPSBANNER/src/oopsabanner_in_star.java
@@ -0,0 +1,13 @@
+public class oopsabanner_in_star {
+
+    public static void main(String[] args) {
+
+        System.out.println(" *****    *****    ******    *****  ");
+        System.out.println("*     *  *     *   *     *  *     * ");
+        System.out.println("*     *  *     *   *     *  *       ");
+        System.out.println("*     *  *     *   ******    *****  ");
+        System.out.println("*     *  *     *   *              * ");
+        System.out.println("*     *  *     *   *        *     * ");
+        System.out.println(" *****    *****    *         *****  ");
+    }
+}
